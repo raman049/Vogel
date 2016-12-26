f1e41abf5021886e67d4f3c26c60f8dd4bc8c9d8
raman049 <raman049@yahoo.com>
2016-12-25 19:15:27 -0800

    Update LastPage.java

--- src/servlets/LastPage.java
+++ src/servlets/LastPage.java
@@ -10,6 +10,10 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -18,9 +22,10 @@ public class LastPage {
 	static FrameClass frame1;
 	static int Height;
 	static int Width;
-	static highScore HIscore;
+	static HighScore HIscore;
 	static BufferedImage background;
 	int score;
+	static Clip clip;
 
 	public class Action {
 
@@ -56,14 +61,22 @@ public class LastPage {
 		frame1 = new FrameClass();
 		Height = frame1.getHeight();
 		Width = frame1.getWidth();
-		frame1.getContentPane().setForeground(new Color(0, 0, 205));
-		frame1.getContentPane().setFont(new Font("Lucida Grande", Font.BOLD, 50));
-		frame1.getContentPane().setBackground(Color.CYAN);
 		// frame.setBackground(Color.CYAN);
+		frame1.getContentPane().setBackground(new Color(0, 102, 255));// background
 		frame1.getContentPane().setLayout(null);
+		// start background music
 
 		try {
-			background = ImageIO.read(new File("InjuredBird.png"));
+			clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(new File("SoundClips/latinHorn.wav")));
+			clip.loop(Clip.LOOP_CONTINUOUSLY);
+			clip.start();
+		} catch (LineUnavailableException | IOException | UnsupportedAudioFileException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		try {
+			background = ImageIO.read(new File("images/InjuredBird.png"));
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -71,45 +84,52 @@ public class LastPage {
 		// High Score
 		String HIscore = "";
 		try {
-			HIscore = highScore.updateHiScore(0).toString();
+			HIscore = HighScore.updateHiScore(0).toString();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		JLabel txtHighScore = new JLabel();
 		txtHighScore.setText("High Score: " + HIscore);
-		txtHighScore.setForeground(Color.MAGENTA);
-		txtHighScore.setFont(new Font("Comic Sans MS", Font.BOLD, 36));
+		txtHighScore.setForeground(new Color(255, 255, 0));// High Score Color
+		txtHighScore.setFont(new Font("Comic Sans MS", Font.BOLD, Height / 15));
 		txtHighScore.setBounds(Width / 2 - 150, Height / 2 - 200, 380, 50);
 		frame1.getContentPane().add(txtHighScore);
 		score = Activity.getFinalScore();
 		JLabel txtYourScore = new JLabel();
 		txtYourScore.setFont(new Font("Comic Sans MS", Font.BOLD, 36));
-		txtYourScore.setText("Your Score:  " +score);
+		txtYourScore.setText("Your Score:  " + score);
 		txtYourScore.setForeground(Color.YELLOW);
 		txtYourScore.setForeground(new Color(0, 0, 128));
-		txtYourScore.setBounds(Width / 2 - 150, Height / 2 - 100, 380, 50);
+		txtYourScore.setBounds(Width / 2 - (Width / 7), Height / 4 - (Height / 5), Width / 2, Height / 2);
 		frame1.getContentPane().add(txtYourScore);
 		// txtYourScore.setColumns(10);
 
 		JButton btnNewButton = new JButton("REPLAY");
-		btnNewButton.setForeground(Color.BLUE);
-		btnNewButton.setFont(new Font("Comic Sans MS", Font.BOLD, 24));
+		btnNewButton.setForeground(new Color(230, 46, 0));// Replay button +
+															// btnNewButton.setForeground(Color.BLUE);
+															// Color +
+															// btnNewButton.setFont(new
+															// Font("Comic Sans
+															// MS", Font.BOLD,
+															// 24));
+		btnNewButton.setFont(new Font("Comic Sans MS", Font.BOLD, Height / 24));
 		btnNewButton.setBounds(Width / 2 - 60, Height / 2 - 60, 120, 50);
+		btnNewButton.setBounds(Width / 2 - (Width / 5), Height / 2 + (Height / 14), Width / 10, Height / 10);
 		frame1.getContentPane().add(btnNewButton);
 		btnNewButton.addActionListener(new CustomActionListener());
 
 		JButton btnNewButton_1 = new JButton("EXIT");
-		btnNewButton_1.setForeground(Color.BLUE);
+		btnNewButton_1.setForeground((new Color(230, 46, 0)));
 		btnNewButton_1.setFont(new Font("Comic Sans MS", Font.BOLD, 24));
-		btnNewButton_1.setBounds(Width / 2 - 60, Height / 2 + 130, 120, 50);
+		btnNewButton_1.setBounds(Width / 2 + (Width / 10), Height / 2 + (Height / 14), Width / 10, Height / 10);
 		frame1.getContentPane().add(btnNewButton_1);
 		// frame1.setResizable(false);
 
 		btnNewButton_1.addActionListener(new EndButtonAction());
 
 		JLabel imagelabel = new JLabel(new ImageIcon(background));
-		imagelabel.setBounds(Width / 2 - 250, Height / 2 - 250, 500, 500);
+		imagelabel.setBounds(Width / 2 - (Width / 4),Height / 5, Width/2, Height);
 		frame1.getContentPane().add(imagelabel);
 	}
 
@@ -118,6 +138,7 @@ public class LastPage {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Start here
+			clip.close();
 			frame1.setVisible(false);
 			// System.out.println("hello");
 			// new Activity();
@@ -132,6 +153,7 @@ public class LastPage {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO exit here
+			clip.close();
 			System.exit(0);
 		}
 
